fa67bec4e29682e64b0a62979ce3eaf0efae8c7e
Ángel Díaz <diazangel9311@gmail.com>
2021-11-16 18:56:30 -0500

    Improve error handling

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -6,10 +6,12 @@ class Api::V1::CarsController < ApplicationController
   def create
     @car = Car.new(car_params)
     if @car.save
-      render json: { message: 'Car succesfully saved!' }
+      render json: { message: 'Car succesfully added.' }, status: :created, code: 201
     else
-      render json: format_errors
+      render json: format_errors, status: :not_acceptable, code: 406
     end
+  rescue StandardError => e
+    render json: { error: e.message }, status: :bad_request, code: 400
   end
 
   private
