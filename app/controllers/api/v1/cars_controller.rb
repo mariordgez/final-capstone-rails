b833c9d049a73c818cb3efdd029a9c6a147e1eed
Ángel Díaz <diazangel9311@gmail.com>
2021-11-16 16:38:55 -0500

    WIP Cars api model

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::CarsController < ApplicationController
+  def index
+    render json: [*Car.all]
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = 
+    if @car.save
+      render json: { message: 'Car saved!' }
+    else
+      render json: { message: 'Car could not be saved'}
+    end
+  end
+
+  private
+
+  def car_params
+    params.permit(:name, :model, :brad, :price, :image_url, :remove)
+  end
 end
