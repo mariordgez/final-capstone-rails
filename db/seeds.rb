ec5694dcda7a951a1347ad302a96e3b9da0bda07
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-11-16 13:44:24 -0400

    Add seed data for cars table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+cars = [
+  {
+    name: 'Sentra',
+    model: 'SV 2020',
+    brand: 'Nissan',
+    price: '20270.0',
+    image_url: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2020-nissan-sentra-120-1574102513.jpg',
+    delete: false
+  },
+
+  {
+    name: 'Civic',
+    model: 'RS 2022',
+    brand: 'Honda',
+    price: '21900.0',
+    image_url: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2022-honda-civic-sedan-110-1623810388.jpg',
+    delete: false
+  }
+
+  {
+    name: '4matic',
+    model: 'e460 2021',
+    brand: 'Mercedes Benz',
+    price: '62750.0',
+    image_url: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2021-mercedes-benz-e450-4matic-sedan-107-1604280340.jpg',
+    delete: false
+  }
+
+  {
+    name: 'Polestar',
+    model: 'v60 t8 2020',
+    brand: 'Volvo',
+    price: '68940.0',
+    image_url: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2020-volvo-v60-t8-polestar-205-hdr-1579105927-mmp-1-1636042387.jpg',
+    delete: false
+  }
+]
+
+cars.each{ |car| Car.create! car }
