b06fdaa18486cf4fdeeeb6828472570fccfbaed5
eroauditore <alfa.fenrir@gmail.com>
2021-11-17 11:04:52 -0600

    Test added to cars endpoint

---
+++ spec/requests/cars_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe 'Cars API', type: :request do
+  let!(:car_test) do
+    FactoryBot.create(:car, name: 'BWM X2', model: '2021', brand: 'BMW', price: 1_000_000.00,
+                            image_url: 'fakeurl.com/bmw', removed: false)
+  end
+  it 'reponse is success' do
+    get "/api/v1/cars/#{car_test.id}"
+    expect(response).to have_http_status(:ok)
+  end
+
+  it 'reponse is hass message with successful' do
+    get "/api/v1/cars/#{car_test.id}"
+    json = JSON.parse(response.body)
+    expect(json['message']).to eql('successful')
+  end
+
+  it 'has two elements data and message' do
+    get "/api/v1/cars/#{car_test.id}"
+    expect(JSON.parse(response.body).size).to eq(2)
+  end
+
+  it 'reponse is not found when car is not found' do
+    get '/api/v1/cars/5'
+    expect(response).to have_http_status(:bad_request)
+  end
+
+  it 'has the attribute removed' do
+    get "/api/v1/cars/#{car_test.id}"
+    json = JSON.parse(response.body, symbolize_names: true)
+    expect(json[:data]).to have_key(:removed)
+  end
+end
