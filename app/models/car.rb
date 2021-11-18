b87b15ba58306921f5ea75a6b88134e087d62c22
Ángel Díaz <diazangel9311@gmail.com>
2021-11-18 13:00:09 -0500

    Remove uniqueness for model
    Only image url is unique

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,7 +1,6 @@
 class Car < ApplicationRecord
-  validates :name, :brand, :price, presence: true, length: { maximum: 20 }
-  validates :model, uniqueness: true, length: { maximum: 20 }
-  validates :image_url, format: { with: %r{\Ahttps*://.+\z}, message: 'Not a valid image URL.' }
+  validates :name, :brand, :model, :price, presence: true, length: { maximum: 20 }
+  validates :image_url, uniqueness: true, format: { with: %r{\Ahttps*://.+\z}, message: 'Not a valid image URL.' }
   validates :removed, inclusion: [false]
   has_many :reservations
 end
