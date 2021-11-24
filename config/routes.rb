c31356dc731f34d324cafe97b199bf0cc7efb467
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-11-23 22:17:56 -0400

    Add update action to cars controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1, defaults: { format: 'json'} do
       resources :users, only: [:index]
-      resources :cars, only: [:index]
+      resources :cars, only: [:index, :update]
       post 'cars/add', to: 'cars#create', as: 'cars_add'
       get '/cars/:id', to: 'cars#show'
     end
