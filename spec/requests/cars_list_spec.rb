d084be154b1bca77d700a141e808c5c91f07798e
Ángel Díaz <diazangel9311@gmail.com>
2021-11-25 11:03:48 -0500

    Add tests for adding a new car to the list

--- spec/requests/cars_list_spec.rb
+++ spec/requests/cars_list_spec.rb
@@ -16,23 +16,23 @@ describe 'Cars API', type: :request do
                             image_url: 'http://fakeurl.com/mazda', removed: false)
   end
   it 'status is ok' do
-    get "/api/v1/cars/"
+    get '/api/v1/cars/'
     expect(response).to have_http_status(:ok)
   end
 
   it 'message is successful' do
-    get "/api/v1/cars/"
+    get '/api/v1/cars/'
     json = JSON.parse(response.body)
     expect(json['message']).to eql('Car list successfully fetched!')
   end
 
   it 'has two elements data and message' do
-    get "/api/v1/cars/"
+    get '/api/v1/cars/'
     expect(JSON.parse(response.body).size).to eq(2)
   end
 
   it 'has six cars in the list' do
-    get "/api/v1/cars/"
+    get '/api/v1/cars/'
     json = JSON.parse(response.body)
     expect((json['data']).size).to eql(6)
   end
